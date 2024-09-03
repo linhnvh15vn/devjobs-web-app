555c33efb15d98058743994a3c4a59ecaf0db0aa
linhnvh15vn <linhnvh15vn@gmail.com>
2024-09-03 22:15:44 +0700

    refactor: replace /api by using json-server

--- src/app/api/jobs/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse, type NextRequest } from 'next/server';
-
-import data from '@/data.json';
-
-export async function GET(req: NextRequest) {
-  return NextResponse.json(data);
-}
